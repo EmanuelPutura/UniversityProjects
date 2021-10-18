e9f8f13919df48f8dd1650771f71f403c9b12e14
916-Putura-Emanuel-Vasile <72196173+916-Putura-Emanuel-Vasile@users.noreply.github.com>
2021-10-18 23:08:13 +0300

    Added Program package

---
+++ Advanced Programming Methods/ToyLanguageInterpretor/src/Model/Program/ProgramState.java
@@ -0,0 +1,37 @@
+package Model.Program;
+
+import Model.DataStructures.IADTDictionary;
+import Model.DataStructures.IADTList;
+import Model.DataStructures.IADTStack;
+import Model.Statements.IStatement;
+import Model.Values.IValue;
+
+public class ProgramState {
+    private IADTStack<IStatement> execution_stack;
+    private IADTDictionary<String, IValue> symbols_table;
+    private IADTList<IValue> out_list;
+
+    public IADTStack<IStatement> executionStack() {
+        return execution_stack;
+    }
+
+    public IADTDictionary<String, IValue> symbolsTable() {
+        return symbols_table;
+    }
+
+    public IADTList<IValue> outList() {
+        return out_list;
+    }
+
+    ProgramState(IADTStack<IStatement> stack, IADTDictionary<String, IValue> dict, IADTList<IValue> list, IStatement statement) {
+        this.execution_stack = stack;
+        this.symbols_table = dict;
+        this.out_list = list;
+        this.execution_stack.push(statement);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ProgramState: execution stack (%s), symbols table (%s), out (%s)", execution_stack.toString(), symbols_table.toString(), out_list.toString());
+    }
+}
---
+++ Advanced Programming Methods/ToyLanguageInterpretor/src/Model/Statements/IStatement.java
@@ -0,0 +1,5 @@
+package Model.Statements;
+
+public interface IStatement {
+
+}
