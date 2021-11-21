1b6ef83f4312d486ca56a949b9c6cd7de2baf453
916-Putura-Emanuel-Vasile <72196173+916-Putura-Emanuel-Vasile@users.noreply.github.com>
2021-11-21 18:39:55 +0200

    Heap Table data structure

--- Advanced Programming Methods/ToyLanguageInterpreter/src/Model/DataStructures/ADTHeapDictionary.java
+++ Advanced Programming Methods/ToyLanguageInterpreter/src/Model/DataStructures/ADTHeapDictionary.java
@@ -1,10 +1,23 @@
 package Model.DataStructures;
 
+import Model.Exceptions.DictionaryException;
 import Model.Values.IValue;
 
-public class ADTHeapDictionary extends ADTDictionary<Integer, IValue> {
+public class ADTHeapDictionary extends ADTDictionary<Integer, IValue> implements IADTHeapDictionary {
     public ADTHeapDictionary() {
         super();
+    }
+
+    @Override
+    public void put(Integer key, IValue value) throws DictionaryException {
+        if (!key.equals(getFirstFreeLocation()))
+            throw new DictionaryException("Invalid heap location!");
+        super.put(key, value);
+    }
+
+    @Override
+    public void put(IValue value) throws DictionaryException {
+        super.put(getFirstFreeLocation(), value);
     }
 
     public int getFirstFreeLocation() {
