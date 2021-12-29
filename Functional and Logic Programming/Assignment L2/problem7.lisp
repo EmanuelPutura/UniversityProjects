f2c0b137fa2cf0bde3e67fab9342074a21410520
Emanuel-Vasile Puțura <emanuelputura@gmail.com>
2021-12-29 19:04:55 +0200

    Update problem7.lisp

--- Functional and Logic Programming/Assignment L2/problem7.lisp
+++ Functional and Logic Programming/Assignment L2/problem7.lisp
@@ -64,4 +64,5 @@
 (level1Main '(a 2 b 2 c 1 i 0 f 1 g 0 d 2 e 1 k 0 h 0) 'G)
 (level1Main '(a 2 b 2 c 1 i 0 f 1 g 0 d 2 e 1 k 0 h 0) 'H)
 (level1Main '(a 2 b 2 c 1 i 0 f 1 g 0 d 2 e 1 k 0 h 0) 'I)
+(level1Main '(a 2 b 2 c 1 i 0 f 1 g 0 d 2 e 1 k 0 h 0) 'K)
 
