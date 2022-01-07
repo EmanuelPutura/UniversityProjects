8e9e072b0205cb95a1be7b72fb5370fcbf3d47b3
916-Putura-Emanuel-Vasile <72196173+916-Putura-Emanuel-Vasile@users.noreply.github.com>
2022-01-07 23:25:45 +0200

    PFL assignment P2 problem 1

---
+++ Functional and Logic Programming/Exam Preparation/FromHomework/Assignment P2/1_b.pl
@@ -0,0 +1,44 @@
+getRet(H, M, H) :-
+    H < M,
+    !.
+getRet(_, M, M).
+
+listMin([], none) :- !.
+listMin([H], H) :- !.
+listMin([H|T], R) :-
+    listMin(T, M),
+    getRet(H, M, R).
+
+swapMin([], _, _, []) :- !.
+swapMin([M|T], E, M, [E|T]) :- !.
+swapMin([H|T], E, M, [H|R]) :-
+    swapMin(T, E, M, R).
+
+swapMinMain([H|T], E, R, M) :-
+    listMin([H|T], M),
+    swapMin([H|T], E, M, R).
+
+/* H >= E always*/
+deleteElemSorted([], _, []) :- !.
+deleteElemSorted([H|T], E, [H|T]) :-
+    H > E,
+    !.
+deleteElemSorted([_|T], E, R) :-
+    deleteElemSorted(T, E, R).
+
+sortList([], []).
+sortList([H|T], [M|RS]) :-
+    swapMinMain([H|T], H, [_|RT], M),
+    sortList(RT, RR),
+    deleteElemSorted(RR, M, RS).
+
+chooseAnswer(H, RT, [RH|RT]) :-
+    is_list(H),
+    !,
+    sortList(H, RH).
+chooseAnswer(H, RT, [H|RT]).
+
+sortSublist([], []).
+sortSublist([H|T], R) :-
+    sortSublist(T, RT),
+    chooseAnswer(H, RT, R).
