c671df49836519e1d564dbe0a9ddefee08ee7116
EmanuelPutura <emanuelputura@gmail.com>
2022-03-18 22:24:24 +0200

    TicTacToe initial game board

--- Web Programming/Assignment4/script.js
+++ Web Programming/Assignment4/script.js
@@ -1,3 +1,16 @@
-function buttonClicked() {
+function buttonClicked(button) {
+    if (button.innerHTML == "X")
+        window.alert("Invalid move!");
+    else
+        button.innerHTML = "X";
 
+    buttonIds = [];
+    for (i = 1; i <= 3; ++i) {
+        for (j = 1; j <= 3; ++j) {
+            buttonIds.push("btn" + String(i) + String(j));
+        }
+    }
+
+    const freeCells = buttonIds.filter((value) => document.getElementById(value).innerHTML == "");
+    document.getElementById(freeCells[0]).innerHTML = "O";
 }
