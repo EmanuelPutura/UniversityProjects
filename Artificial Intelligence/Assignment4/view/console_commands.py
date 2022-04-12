b03a569f9630754294635c652ac73ee39f3a8215
EmanuelPutura <emanuelputura@gmail.com>
2022-04-12 10:02:42 +0300

    Added console parameters setup

--- Artificial Intelligence/Assignment4/view/console_commands.py
+++ Artificial Intelligence/Assignment4/view/console_commands.py
@@ -1,9 +1,4 @@
-import timeit
-
-import matplotlib.pyplot
-import numpy
-
-from utils.utils import START_POSITION, RUNNING_TIMES
+from utils.utils import START_POSITION, MAP_LENGTH
 from view.gui import movingDrone
 
 
@@ -11,10 +6,10 @@ class ConsoleCommands:
     def __init__(self, controller, randomGenerator):
         self.__solutionPath = []
         self.__controller = controller
-        self.__randomGenerator = randomGenerator
 
-        self.__populationSize = 2
-        self.__individualSize = 5
+        self.__droneStartPosition = (0, 0)
+        self.__droneEnergy = 10
+        self.__sensors = [(1, 2), (6, 7), (15, 11), (10, 10), (19, 19)]
 
     def randomMapCommand(self):
         self.__controller.generateRandomMap()
@@ -31,16 +26,49 @@ class ConsoleCommands:
         movingDrone(self.__controller.getMap(), [START_POSITION], 0, False)
 
     def setDroneStartPositionCommand(self):
-        pass
+        startX = int(input("Please enter the X start coordinate of the drone ([0; : {}]): ".format(MAP_LENGTH - 1)))
+        startY = int(input("Please enter the Y start coordinate of the drone ([0; : {}]): ".format(MAP_LENGTH - 1)))
+
+        if not (0 <= startX < MAP_LENGTH and 0 <= startY < MAP_LENGTH):
+            print("({}, {}) is not a valid drone position!".format(startX, startY))
+
+        self.__droneStartPosition = (startX, startY)
+
+        # TODO
+        print(self.__droneStartPosition)
 
     def setDroneEnergyCommand(self):
-        pass
+        self.__droneEnergy = int(input("Please enter the energy level of the drone: "))
+
+        # TODO
+        print(self.__droneEnergy)
 
     def setSensorsPositionCommand(self):
-        pass
+        sensors = []
+        sensorsNo = int(input("Please enter the number of sensors on the map: "))
+        getCoordinates = lambda: (
+            int(input("Please enter the X coordinate of the sensor ([0; : {}]): ".format(MAP_LENGTH - 1))),
+            int(input("Please enter the Y coordinate of the sensor ([0; : {}]): ".format(MAP_LENGTH - 1)))
+        )
+
+        for _ in range(sensorsNo):
+            (x, y) = getCoordinates()
+
+            while not (0 <= x < MAP_LENGTH and 0 <= y < MAP_LENGTH):
+                print("({}, {}) is not a valid drone position!".format(x, y))
+                (x, y) = getCoordinates()
+
+            sensors.append((x, y))
+
+        self.__sensors = sensors
+
+        # TODO
+        print(self.__sensors)
 
     def runAlgorithmCommand(self):
+        # TODO
         pass
 
     def showDronePathCommand(self):
+        # TODO
         pass
