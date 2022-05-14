47432a621a55e2c2d823af5666f3ae7a955e3beb
EmanuelPutura <emanuelputura@gmail.com>
2022-05-15 00:37:56 +0300

    Puzzle setup

--- Second Year/Second Semester/Web Programming/Assignment8-Jsp-JavaServlets/src/main/webapp/scripts/pieces.js
+++ Second Year/Second Semester/Web Programming/Assignment8-Jsp-JavaServlets/src/main/webapp/scripts/pieces.js
@@ -1,3 +1,8 @@
+const VARIATIONS = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+
+let emptyCellX;
+let emptyCellY;
+
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
 
@@ -27,12 +32,40 @@ function coordinatesSetup() {
     return shuffle(coordinates);
 }
 
+function isNeighbourWithEmptyCell(cellX, cellY) {
+    for (let index = 0; index < VARIATIONS.length; ++index) {
+        if (cellX + VARIATIONS[index][0] === emptyCellX && cellY + VARIATIONS[index][1] === emptyCellY) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function movePiece(cellX, cellY) {
+    console.log(isNeighbourWithEmptyCell(cellX, cellY));
+}
+
 function setPieces() {
     let coordinates = coordinatesSetup();
 
     for (let index = 0; index < 9; ++index) {
         let pieceDiv = document.createElement("div");
-        pieceDiv.style.backgroundImage = "url(../resources/images/image" + coordinates[index][0].toString() + coordinates[index][1].toString() + ".jpg)";
+        pieceDiv.id = "piece" + coordinates[index][0].toString() + coordinates[index][1].toString();
+        pieceDiv.draggable = true;
+        // pieceDiv.onclick = function() {
+        //     console.log(this);
+        //     movePiece(Math.floor(index / 3), index % 3);
+        // };
+
+        if (coordinates[index][0] === 2 && coordinates[index][1] === 0) {
+            emptyCellX = Math.floor(index / 3)
+            emptyCellY = index % 3;
+        }
+        else {
+            pieceDiv.style.backgroundImage = "url(../resources/images/image" + coordinates[index][0].toString() + coordinates[index][1].toString() + ".jpg)";
+        }
+
         document.getElementById("puzzle-table").appendChild(pieceDiv)
     }
 }
