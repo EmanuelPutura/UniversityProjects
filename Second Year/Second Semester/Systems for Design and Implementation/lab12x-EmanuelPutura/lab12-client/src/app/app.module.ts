d338fd640fa8fc282b648edea4a1a6db24a8034d
EmanuelPutura <emanuelputura@gmail.com>
2022-05-17 12:44:15 +0300

    Added navigation bar

---
+++ Second Year/Second Semester/Systems for Design and Implementation/lab12x-EmanuelPutura/lab12-client/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
